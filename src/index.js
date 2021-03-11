c2d63ac068b968b4250515b898f71e52288a0d2e
96tm <khabcity@gmail.com>
2021-03-11 21:01:33 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (!n) return n;
+    n = Math.abs(n);
+    let power = Math.trunc(Math.log10(n));
+    let result = 0;
+    while (n) {
+        let digit = n % 10;
+        result += digit * 10 ** power;
+        n = Math.trunc(n / 10);
+        power -= 1;
+    }
+    return result;
+};
